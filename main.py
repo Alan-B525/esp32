9ce7fb4628d4d7f9bcca4e22041da1efd6219977
Alan-B525 <alannauhel@gmail.com>
2024-03-19 17:01:44 -0300

    puebra sensor

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import dht, machine
+import dht, machine, time
 
-d = dht.DHT22(machine.Pin(13))
-d.measure()
-d.temperature()
-d.humidity()
+d = dht.DHT22(machine.Pin(25))
+
+
+
+while True:
+    measure = d.measure()
+    temp = d.temperature()
+    humedad = d.humidity()
+
+    print('------Datos------')
+    print('Measure', measure)
+    print('Temperatura', temp)
+    print('Humedad', humedad)
+    print('------------------')
+    time.sleep_ms(3000)

5295e683832c8bfd107097c2c4d62882f5fd01fb
Alan-B525 <alannauhel@gmail.com>
2024-04-06 18:01:29 -0300

    se mejoro el codigo para probar el rele

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from machine import Pin
 import dht
+import time
 
 d = dht.DHT22(Pin(25))
-d.measure()
-temperatura=d.temperature()
-print(f"la temperatura actual es de {temperatura} °C")
-humedad=d.humidity()
-print(f"la humedad actual es de {temperatura} %")
+rele = Pin(14, Pin.OUT)
+rele.value(0)
+
+while(True):
+    rele.value(not rele.value())
+    d.measure()
+    temperatura=d.temperature()
+    print(f"la temperatura actual es de {temperatura} °C")
+    humedad=d.humidity()
+    print(f"la humedad actual es de {humedad} %")
+    time.sleep(1)
